931bcd84becc0389ecb39fc9be22b43db0ad51d5
Will保哥 <doggy.huang@gmail.com>
2017-03-12 13:28:01 +0800

    11 練習擴充實體資料模型 ( 使用 EFGeneratePartialClass.tt 程式碼產生器範本 ) - Step 2
    從 Product.cs 搬移必要的輸入驗證屬性與模型驗證到 Partial Class 中

--- MVC5Course/Models/Product.Partial.cs
+++ MVC5Course/Models/Product.Partial.cs
@@ -2,8 +2,9 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
+    using System.ComponentModel;
     using System.ComponentModel.DataAnnotations;
-    
+
     [MetadataType(typeof(ProductMetaData))]
     public partial class Product
     {
@@ -15,9 +16,16 @@ namespace MVC5Course.Models
         public int ProductId { get; set; }
         
         [StringLength(80, ErrorMessage="欄位長度不得大於 80 個字元")]
+        [Required(ErrorMessage = "請輸入商品名稱 ({0})")]
+        [DisplayName("商品名稱")]
         public string ProductName { get; set; }
+        [Required]
+        [Range(10, 99999999, ErrorMessage = "金額設定錯誤")]
+        [DisplayFormat(DataFormatString = "{0:N0}")]
         public Nullable<decimal> Price { get; set; }
+        [Required]
         public Nullable<bool> Active { get; set; }
+        [Required]
         public Nullable<decimal> Stock { get; set; }
     
         public virtual ICollection<OrderLine> OrderLine { get; set; }
