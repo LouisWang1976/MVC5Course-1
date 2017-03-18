98629a4fd1f77d5530de219200d730d62dae0b79
Will保哥 <doggy.huang@gmail.com>
2017-03-18 16:07:30 +0800

    24 練習多筆資料的模型繫結
    重構程式碼，將重複的程式碼獨立成方法

--- MVC5Course/Controllers/ProductsController.cs
+++ MVC5Course/Controllers/ProductsController.cs
@@ -17,25 +17,29 @@ namespace MVC5Course.Controllers
         // GET: Products
         public ActionResult Index(string sortBy, string keyword, int pageNo = 1)
         {
-            var data = repoProduct.All().AsQueryable();
+            DoSearchOnIndex(sortBy, keyword, pageNo);
 
-            if (!String.IsNullOrEmpty(keyword))
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Product[] data,string sortBy, string keyword, int pageNo = 1)
+        {
+            if (ModelState.IsValid)
             {
-                data = data.Where(p => p.ProductName.Contains(keyword));
+                foreach (var item in data)
+                {
+                    var prod = repoProduct.Find(item.ProductId);
+                    prod.ProductName = item.ProductName;
+                    prod.Price = item.Price;
+                    prod.Stock = item.Stock;
+                    prod.Active = item.Active;
+                }
+                repoProduct.UnitOfWork.Commit();
+                return RedirectToAction("Index");
             }
 
-            if (sortBy == "+Price")
-            {
-                data = data.OrderBy(p => p.Price);
-            }
-            else
-            {
-                data = data.OrderByDescending(p => p.Price);
-            }
-
-            ViewBag.keyword = keyword;
-
-            ViewData.Model = data.ToPagedList(pageNo, 10);
+            DoSearchOnIndex(sortBy, keyword, pageNo);
 
             return View();
         }
@@ -135,5 +139,29 @@ namespace MVC5Course.Controllers
             repoProduct.UnitOfWork.Commit();
             return RedirectToAction("Index");
         }
+
+        private void DoSearchOnIndex(string sortBy, string keyword, int pageNo)
+        {
+            var all = repoProduct.All().AsQueryable();
+
+            if (!String.IsNullOrEmpty(keyword))
+            {
+                all = all.Where(p => p.ProductName.Contains(keyword));
+            }
+
+            if (sortBy == "+Price")
+            {
+                all = all.OrderBy(p => p.Price);
+            }
+            else
+            {
+                all = all.OrderByDescending(p => p.Price);
+            }
+
+            ViewBag.keyword = keyword;
+
+            ViewData.Model = all.ToPagedList(pageNo, 10);
+        }
+
     }
 }
